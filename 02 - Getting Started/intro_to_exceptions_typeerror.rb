9c0d316328e496d850fae75c3dc33a265eb93630
Edwin Cardenas <eacardenase@gmail.com>
2024-05-26 14:31:14 -0500

    feat: intro to exceptions

---
+++ 02 - Getting Started/intro_to_exceptions_typeerror.rb
@@ -0,0 +1,8 @@
+puts 3 + 4
+puts "Miss" + " " + "Fortune"
+
+puts
+
+# TypeErrors
+# puts "2" + 1 # no implicit conversion of Integer into String
+puts 1 + "2" # String can't be coerced into Integer
